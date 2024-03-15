af61cdc74162e735f4ef7bd772ab4d1a7d885f9d
Dan <stickbugdanny@gmail.com>
2024-03-15 17:14:19 +0200

    Create App.xaml.cs

---
+++ CSharp_Lab2_ParsonApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CSharp_Lab2_ParsonApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+
+    public partial class App : Application
+    {
+
+        private bool _isMainWindowShown = false;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+           base.OnStartup(e);
+
+            if (!_isMainWindowShown)
+            {
+                // Set up ViewModel and bind it to the View
+                var viewModel = new PersonViewModel();
+                var mainWindow = new MainWindow();
+                mainWindow.DataContext = viewModel;
+                mainWindow.Show();
+                _isMainWindowShown = true;
+            }
+            
+        }
+    }
+}
+
+
