de57032d83f6f75a352fe6b13fb2cba4409c7d90
Dan <stickbugdanny@gmail.com>
2024-03-16 11:47:16 +0200

    Update PersonViewModel.cs

--- CSharp_Lab2_ParsonApp/PersonViewModel.cs
+++ CSharp_Lab2_ParsonApp/PersonViewModel.cs
@@ -9,10 +9,66 @@ namespace CSharp_Lab2_ParsonApp
     public class PersonViewModel : INotifyPropertyChanged
     {
         // Properties
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        private string _firstName;
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                if (_firstName != value)
+                {
+                    _firstName = value;
+                    OnPropertyChanged(nameof(FirstName));
+                    OnPropertyChanged(nameof(CanProceed));
+                }
+            }
+        }
+
+        private string _lastName;
+        public string LastName
+        { 
+            get => _lastName;   
+            set
+            {
+                if (_lastName != value)
+                { 
+                    _lastName = value; 
+                    OnPropertyChanged(nameof(LastName));
+                    OnPropertyChanged(nameof(CanProceed));
+                }
+            }
+        }
+
+        private string _email;
+        public string Email
+        { 
+            get => _email;
+            set
+            {
+                if (_email != value)
+                {
+                    _email = value;
+                    OnPropertyChanged(nameof(Email));
+                    OnPropertyChanged(nameof(CanProceed));
+                }
+            }
+        }
+
+
+        private DateTime _dateOfBirth;
+        public DateTime DateOfBirth
+        {
+            get => _dateOfBirth;
+            set
+            {
+                if (_dateOfBirth != value)
+                {
+                    _dateOfBirth = value;
+                    OnPropertyChanged(nameof(DateOfBirth));
+                    OnPropertyChanged(nameof(CanProceed));
+                }
+            }
+        }
 
         //Commands
         public ICommand ProceedCommand { get; }
@@ -52,11 +108,7 @@ namespace CSharp_Lab2_ParsonApp
                 return;
             }
 
-            bool isBirthday = DateOfBirth.Month == DateTime.Today.Month && DateOfBirth.Day == DateTime.Today.Day;
-            if (isBirthday)
-            {
-                MessageBox.Show("Happy birthday!", "Birthday", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
+           
 
             var person = new Person(FirstName, LastName, Email, DateOfBirth);
             await Task.Run(() =>
@@ -65,6 +117,9 @@ namespace CSharp_Lab2_ParsonApp
                 bool isAdult = person.IsAdult;
                 string sunSign = person.SunSign;
                 string chineseSign = person.ChineseSign;
+                bool isBirthday = person.IsBirthday;
+
+                /**TODO: CHANGE SEPARATE MESSAGE BOX INTO TEXT APPEARING IN SAME WINDOW*/
 
                 //Print out values
                 MessageBox.Show($"FirstName: {person.GetFirstName()}\n" +
