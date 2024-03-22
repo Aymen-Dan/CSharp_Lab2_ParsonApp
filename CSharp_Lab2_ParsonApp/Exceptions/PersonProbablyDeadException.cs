8cd3fc4ae19a17d61a1eb0e9ffc1cb47b3dbd717
Dan <stickbugdanny@gmail.com>
2024-03-23 01:04:48 +0200

    Create PersonProbablyDeadException.cs

---
+++ CSharp_Lab2_ParsonApp/Exceptions/PersonProbablyDeadException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Lab2_ParsonApp
+{
+    class PersonProbablyDeadException : Exception
+    {
+        public PersonProbablyDeadException() : base("Invalid date of birth: Unrealistic age")
+        {
+        }
+
+        public PersonProbablyDeadException(string message) : base(message)
+        {
+        }
+
+        public PersonProbablyDeadException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        //Helper method to check if a person's date of birth is in the future
+
+        //TO-DO: REDO THE CHECK
+        public static void CheckIfFarPastDateOfBirth(DateTime dateOfBirth)
+        {
+            int age = DateTime.Today.Year - dateOfBirth.Year;
+            if (age > 135)
+            {
+                throw new PersonProbablyDeadException();
+            }
+        }
+    }
+}
+
