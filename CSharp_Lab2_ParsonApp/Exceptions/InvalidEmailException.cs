df0cf46509d5fefa70d5e4fc48ead76063f641fc
Dan <stickbugdanny@gmail.com>
2024-03-23 01:04:44 +0200

    Create InvalidEmailException.cs

---
+++ CSharp_Lab2_ParsonApp/Exceptions/InvalidEmailException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Lab2_ParsonApp
+{
+    class InvalidEmailException : Exception
+    {
+        public InvalidEmailException() : base("Invalid email format. Expected format: myemail@domain.com")
+        {
+        }
+
+        public InvalidEmailException(string message) : base(message)
+        {
+        }
+
+        public InvalidEmailException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        //Helper method to check if a person's date of birth is in the future
+
+        //TO-DO: REDO THE CHECK
+        public static void CheckEmailIsValid(string email)
+        {
+            if (email !=  "myemail@domain.com")
+            {
+                throw new InvalidEmailException();
+            }
+        }
+    }
+}
